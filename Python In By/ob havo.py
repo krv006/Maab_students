91a924cea4b938c67e8551cd7c320feeeb7f254c
krv006 <rvkamronbek@gmail.com>
2025-05-09 14:49:40 +0500

    change

--- Python In By/ob havo.py
+++ Python In By/ob havo.py
@@ -2,34 +2,53 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pandas as pd
 
-# Web driver'ni ishga tushurish
 driver = webdriver.Chrome()
 driver.get("https://hydromet.uz/")
-
-# Malumotlar saqlash uchun bo'sh lug'at yaratish
 data = {
     "city": [],
     "day": [],
     "day_part": [],
     "degree": []
 }
+options = driver.find_elements(By.CSS_SELECTOR, "select option")
+for option in options:
+    try:
+        city = option.text
+        option.click()
+        blocks = driver.find_elements(By.CSS_SELECTOR, ".weather-widget__forecast__table .day")
+        for block in blocks:
+            ls = block.text.split("\n")
+            data['city'].append(city)
+            data['day'].append(ls[0])
+            data["day_part"].append(ls[1])
+            data['degree'].append(ls[2].replace("…", "-"))
+    except:
+        break
 
-# Shaharlar ro'yxatiga o'tish
-for i in range(1, 17):
-    option = driver.find_element(By.XPATH, f"/html/body/div[2]/div[1]/header/div[2]/div/select/option[{i}]")
-    option.click()
-    city = option.text
-    for i in range(1, 7):
-        day = driver.find_element(By.XPATH, f"/html/body/div[2]/div[1]/header/div[2]/div/div[4]/div/div[{i}]/h4").text
-        day_part = driver.find_element(By.XPATH, f"/html/body/div[2]/div[1]/header/div[2]/div/div[4]/div/div[{i}]//span").text
-        degree = driver.find_element(By.XPATH, "//span[@class='temp']").text
-        data['city'].append(option.text)
-        data['day'].append(day)
-        data["day_part"].append(day_part)
-        data['degree'].append(degree)
+city_region = {
+    "tashkent": "Tashkent",
+    "gulistan": "Sirdaryo viloyati",
+    "urgench": "Xorazm",
+    "nukus": "Qoraqolpog'iston respublikasi",
+    "bukhara": "Bukhara",
+    "navoiy": "Navoiy",
+    "samarkand": "samarkand",
+    "jizzakh": "jizzakh",
+    "qarshi": "qashqadaryo",
+    "termez": "surkhandarya",
+    "fergana": "fergana",
+    "namangan": "namangan",
+    "andijan": "andijan",
+    "nurafshon": "tashkent viloyati",
+    "kamchik": "andijon",
+    "chimgan": "tashkent"
+}
+
+region = lambda x: city_region[x].capitalize()
 
 df = pd.DataFrame(data)
 
-df.to_excel("forecast.xlsx", index=False)
+df['region'] = [region(x.lower()) for x in df['city']]
 
+df.to_excel("forecast.xlsx", index=False)
 driver.quit()
