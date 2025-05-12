a5ca8540c6c649c3b40173fc6130186ca327b0d6
krv006 <rvkamronbek@gmail.com>
2025-05-12 07:59:51 +0500

    change

--- Karantin/testy.py
+++ Karantin/testy.py
@@ -1,13 +1,12 @@
+import pandas as pd
+import requests
 import json
 import os
 
-import pandas as pd
-import requests
-
 TOKEN = "040454c87ba748c3d00298fa18a803f631c6ddf1"
 
-df = pd.read_excel("FarmerTin.xlsx")
-
+# Excel fayldan TINlarni olish
+df = pd.read_excel("Test.xlsx")
 inn_list = (
     df['Tin']
     .dropna()
@@ -21,14 +20,17 @@ headers = {
     "Authorization": f"Token {TOKEN}"
 }
 
+# Eski JSON faylni o‘qish (agar mavjud bo‘lsa)
 if os.path.exists("results.json"):
     with open("results.json", "r", encoding="utf-8") as f:
         old_results = json.load(f)
 else:
     old_results = []
 
+# Eski inn lar ro‘yxati
 existing_inns = {entry['inn'] for entry in old_results}
 
+# Yangi ma'lumotlarni yig‘ish
 new_results = []
 
 for inn in inn_list:
@@ -47,8 +49,10 @@ for inn in inn_list:
     except requests.RequestException as e:
         print(f"[{inn}] - xato : {e}")
 
+# Eski va yangi ma'lumotlarni birlashtirish
 all_results = old_results + new_results
 
+# JSON faylga saqlash
 with open("results.json", "w", encoding="utf-8") as f:
     json.dump(all_results, f, ensure_ascii=False, indent=4)
 
