c86da61e0b04a32d9066e111aceb5cd63c363e5a
krv006 <rvkamronbek@gmail.com>
2025-07-03 14:55:33 +0500

    add academ task

---
+++ legal.py
@@ -0,0 +1,7 @@
+"""
+1.	Son juft yoki toq ekanligini aniqlash:
+    Foydalanuvchidan son kiritishni so‘rang va uning juft yoki toq ekanligini aniqlaydigan dastur yozing.
+
+2. 2 ta list ga juft toq sonlardi bolib chiqish (1-100)
+"""
+
