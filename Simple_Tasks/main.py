f397b632a686f3f0f4126c674e5bb1a8a406aeb7
krv006 <rvkamronbek@gmail.com>
2025-07-11 15:59:53 +0500

    add

--- Simple_Tasks/main.py
+++ Simple_Tasks/main.py
@@ -1,9 +1,15 @@
-s = "Hello World"
+# s = "Hello World"
+#
+# # print(s.upper())
+# # print(s.lower())
+#
+# # print(s[::-1])
+#
+# s = s.swapcase()
+# print(s)
 
-# print(s.upper())
-# print(s.lower())
 
-# print(s[::-1])
-
-s = s.swapcase()
-print(s)
+# cars = ('a')
+# l = list(cars)
+# l.append('b')
+# print(l)
