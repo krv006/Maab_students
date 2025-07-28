d44c68d38c64f0240116133ecedf29e4ef0077dd
krv006 <rvkamronbek@gmail.com>
2025-07-28 11:40:12 +0500

    change

--- data_types.py
+++ data_types.py
@@ -3,6 +3,12 @@ int, str, float, set, dict, list, tuple
 """
 
 import pandas as pd
+
 df = pd.read_excel("simple.xlsx")
 df1 = df.to_csv("simple.csv")
 print(df1)
+
+a = set()
+# a.add('as')
+a.add([1, 2, 3])
+print(a)
