343d1a1d404f0d207c68843a4ddd05fa40c678b7
krv006 <rvkamronbek@gmail.com>
2025-08-06 15:15:14 +0500

    add

--- Shohjahon_American/american.py
+++ Shohjahon_American/american.py
@@ -6,7 +6,7 @@ url = "https://nbreadymix.jonel.com:8443/goldgate/v1/order"
 
 params = {
     "AuthToken": "A7066CBF-1791-4C39-8968-B823A26953D3",
-    "OrderId": 459117
+    "OrderId": 455398
 }
 
 response = requests.get(url, params=params)
@@ -14,8 +14,8 @@ response = requests.get(url, params=params)
 if response.status_code == 200:
     print("Success: 200")
     data = response.json()
-    with open("order_data.json", "w", encoding="utf-8") as f:
+    with open("order_data2.json", "w", encoding="utf-8") as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
-    print("Order ma'lumotlari 'order_data.json' fayliga yozildi.")
+    print("Order ma'lumotlari 'order_data2.json' fayliga yozildi.")
 else:
     print(f"Xatolik: {response.status_code} - {response.text}")
